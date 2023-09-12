ed33dcbccca9927d1d66cc11edfb78c814761043
Gabriel Catani <gabrielm.catani@gmail.com>
2023-09-12 17:39:18 -0300

    feat: add round function to play a round of the game

--- script.js
+++ script.js
@@ -4,15 +4,60 @@ function getComputerChoice() {
     const value = Math.floor(Math.random() * 9);
     let choice;
     if(value <= 2) {
-        choice = "Rock";
+        choice = "rock";
     }
     else if (value > 2 && value <= 5) {
-        choice = "Paper";
+        choice = "paper";
     }
     else {
-        choice = "Scissors";
+        choice = "scissors";
     }
     
     return choice;
 }
 
+
+function round(playerSelection, computerSelection) {
+    let playerWon = false;
+    let computerWon = false;
+
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "scissors") {
+            playerWon = true;
+        }
+        else if (computerSelection === "paper") {
+            computerWon = true;
+        } 
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            playerWon = true;
+        }
+        else if (computerSelection === "scissors") {
+            computerWon = true;
+        } 
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            playerWon = true;
+        }
+        else if (computerSelection === "rock") {
+            computerWon = true;
+        } 
+    }
+
+    if (!playerWon && !computerWon) {
+        console.log("Draw! both played " + computerSelection);
+    }
+    else if(playerWon) {
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    }
+}
+
+
+round("rock", getComputerChoice());
+round("paper", getComputerChoice());
+round("scissors", getComputerChoice());
